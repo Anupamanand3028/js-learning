96f11eb98369e3f7034969b895fbde3792de1da9
Anupam Anand <44666798+Anupamanand3028@users.noreply.github.com>
2024-03-02 13:30:38 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountEmail = "anupam123@gmail.com"
+var accountPassword = "123451"
+
+let accountState
+
+
+accountCity = "Ranchi"  // not recommended to use
+
+// accountId = 2  // not allowed
+accountEmail = "aa@gmail.com"
+accountPassword = "4444"
+accountCity = "bengaluru"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser and the code is not inside any document.
+
+let name = "Anupam"
+let age = 18
+let isLoggedIn = false
+
+// numbers => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standalone value    | type => object
+// undefined                  | type => undefined
+// symbol => unique
+
+// object 
+
+console.log(typeof name)
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null);
